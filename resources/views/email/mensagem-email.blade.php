a4bbd0e6779e75cba7740eb65671fd5af7082d23
Diego B Morais <128264029+diegobmorais@users.noreply.github.com>
2024-03-15 11:10:07 -0300

    cria publicação de email utilizando markdown e envia email para destinatario da rota

---
+++ resources/views/email/mensagem-email.blade.php
@@ -0,0 +1,18 @@
+<@component('mail::message')
+
+
+    # Introdução
+
+    What is Lorem Ipsum?
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+    standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
+
+
+   @component('mail::button', ['url' => ''])
+        enviar
+   @endcomponent
+
+    Obrigado!!,
+    {{ config('app.name') }}
+
+@endcomponent
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TarefaController;
+use App\Mail\MensagemMail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,3 +13,11 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('tarefas','App\Http\Controllers\TarefaController')->middleware('auth');
+
+Route::
+get('/enviar-email', function(){
+
+    return new MensagemMail();
+    //Mail::to('diegobatistademorais@gmail.com')->send(new MensagemMail());
+    //return 'email enviado com sucesso!';
+});
